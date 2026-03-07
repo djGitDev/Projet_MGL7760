b043fc5ff3dc274150edf93c8e058c23c7105576
Alex Ouedraogo <ouedraogo.alex_ali_fulgence@courrier.uqam.ca>
2026-03-06 19:58:51 -0500

    Écriture de test de OutilService.java

---
+++ src/test/java/com/example/projet3/service/OutilServiceTest.java
@@ -0,0 +1,29 @@
+package com.example.projet3.service;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.projet3.repository.OutilRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class OutilServiceTest {
+
+    @Mock
+    private OutilRepository outilRepository;
+
+    @InjectMocks
+    private OutilService outilService;
+
+    @Test
+    void testGetOutilByIdNotFound() {
+        Long id = -1111L;
+        assertThrows(RuntimeException.class, () -> {
+            outilService.getOutilById(id);
+        });
+    }
+}
