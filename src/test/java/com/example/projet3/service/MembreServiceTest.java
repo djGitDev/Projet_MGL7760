2e91fe796583b06e7974570618706dae4f7e1cc3
Alex Ouedraogo <ouedraogo.alex_ali_fulgence@courrier.uqam.ca>
2026-03-13 18:59:43 -0400

    Suppression d'imports inutiles dans les classes de test

--- src/test/java/com/example/projet3/service/MembreServiceTest.java
+++ src/test/java/com/example/projet3/service/MembreServiceTest.java
@@ -7,7 +7,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.List;
 
-import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
